012506276259c90a1be79704a2cffc5264fcf114
julle0123 <julle03@naver.com>
2025-06-15 17:27:04 +0900

    first commit

---
+++ src/main/java/com/Marshmallow/mallang/MallangApplication.java
@@ -0,0 +1,13 @@
+package com.Marshmallow.mallang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Marshmallow/mallang/MallangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Marshmallow.mallang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
